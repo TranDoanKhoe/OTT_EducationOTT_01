cac1fd3905907d943de4cf924899f9f0325a8488
Van <dothanhvan651@gmail.com>
2026-03-03 11:10:22 +0700

    chore(frontend): keep summary output stable

--- Frontend/src/utils/ThanhVanMarker.js
+++ Frontend/src/utils/ThanhVanMarker.js
@@ -1,6 +1,6 @@
-export const thanhVanRevision = 6;
+export const thanhVanRevision = 7;
 
-export const thanhVanHistory = ['commit-1', 'commit-2', 'commit-3', 'commit-4', 'commit-5', 'commit-6'];
+export const thanhVanHistory = ['commit-1', 'commit-2', 'commit-3', 'commit-4', 'commit-5', 'commit-6', 'commit-7'];
 
 export const getThanhVanSummary = () => {
     return ${thanhVanRevision}:;
