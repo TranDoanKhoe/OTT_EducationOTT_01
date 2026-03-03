b943d622020772af9de6ca7ea9427ed5ec6f343c
Van <dothanhvan651@gmail.com>
2026-03-03 11:10:21 +0700

    chore(frontend): initialize revision snapshot tracking

--- Frontend/src/utils/ThanhVanMarker.js
+++ Frontend/src/utils/ThanhVanMarker.js
@@ -1,6 +1,6 @@
-export const thanhVanRevision = 1;
+export const thanhVanRevision = 2;
 
-export const thanhVanHistory = ['commit-1'];
+export const thanhVanHistory = ['commit-1', 'commit-2'];
 
 export const getThanhVanSummary = () => {
     return ${thanhVanRevision}:;
