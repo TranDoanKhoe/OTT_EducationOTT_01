cc27dc7a860c4aafee65ab2a40117a2d5c10dbf1
TranDoanKhoe <doankhoe0809@gmail.com>
2026-03-20 22:25:08 +0700

    feat(backend): add app entrypoint and core service interfaces

---
+++ Backend/src/main/java/vn/edu/iuh/fit/ott_education_be/OttAppBeApplication.java
@@ -0,0 +1,17 @@
+package vn.edu.iuh.fit.ott_education_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableMongoAuditing
+@EnableAsync
+public class OttAppBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OttAppBeApplication.class, args);
+    }
+
+}
