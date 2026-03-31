dd773bc9b7bd5556bbb4f00daaad577176463a02
DanHuy <danhuymsk04@gmail.com>
2026-03-31 15:25:25 +0700

    BE: Note duplicate user exception in profile flow

--- Backend/src/main/java/vn/edu/iuh/fit/ott_education_be/exception/DulicatedUserException.java
+++ Backend/src/main/java/vn/edu/iuh/fit/ott_education_be/exception/DulicatedUserException.java
@@ -1,5 +1,7 @@
 package vn.edu.iuh.fit.ott_education_be.exception;
 
+// Raised when username, email, or phone already exists.
+
 public class DulicatedUserException extends RuntimeException {
     public DulicatedUserException(String message) {
         super(message);
