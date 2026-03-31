b41c4ca2842b9d11896d3977b02cc4686af2dc16
DanHuy <danhuymsk04@gmail.com>
2026-03-31 15:23:24 +0700

    BE: Note unauthorized exception for auth validation

--- Backend/src/main/java/vn/edu/iuh/fit/ott_education_be/exception/UnauthorizedException.java
+++ Backend/src/main/java/vn/edu/iuh/fit/ott_education_be/exception/UnauthorizedException.java
@@ -3,6 +3,8 @@ package vn.edu.iuh.fit.ott_education_be.exception;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+// Raised when authentication or token validation fails.
+
 @Getter
 public class UnauthorizedException extends RuntimeException {
     private final HttpStatus status;
